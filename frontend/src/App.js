a55c126a81410401dc72a07865c1baaf47908ecb
arifrumel400 <arif.rumel400@gmail.com>
2021-03-10 23:09:27 +0600

    frontend- footer and home page create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Home/>
+      <Footer/>
     </div>
   );
 }
