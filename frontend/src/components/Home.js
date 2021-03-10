d9e2f0731786ea5d944794979a85c1caf6410069
arifrumel400 <arif.rumel400@gmail.com>
2021-03-10 23:42:48 +0600

    create custom title for every page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
+import MetaData from './layout/MetaData'
 
 const Home = () => {
     return (
        
         <Fragment>
 
+           <MetaData title={'Home page'}/>
           <h1 id="products_heading">Latest Products</h1>
 
             <section id="products" className="container mt-5">
