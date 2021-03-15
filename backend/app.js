8978b86f0002f14ac203dd0c4c7eaf6a48a81670
arifrumel400 <arif.rumel400@gmail.com>
2021-03-15 22:42:23 +0600

    first test route created

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,10 @@ const express = require('express')
 const app = express()
 
 
+app.use(express.json())
+  
+
+const products = require('./routes/product')
+app.use('/', products)
+
 module.exports = app 
