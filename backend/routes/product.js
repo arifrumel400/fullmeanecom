c0081684c7f150f1ebb7b3fdc96702a0ff74eff4
arifrumel400 <arif.rumel400@gmail.com>
2021-03-23 17:27:01 +0600

    all product route done

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,17 @@
 const express = require('express')
-const { getProduct, newProduct} = require('../controllers/productController')
+const { getProduct, newProduct,getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController')
 const router = express.Router()
 
 
 
 router.route('/products').get(getProduct)
 router.route('/product/new').post(newProduct)
+router.route('/product/:id').get(getSingleProduct)
+
+router.route('/product/:id').put(updateProduct)
+
+router.route('/product/:id').delete(deleteProduct)
+
 
 
 module.exports = router
