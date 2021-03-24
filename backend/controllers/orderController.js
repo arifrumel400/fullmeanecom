dec01d1144d31592181c45b9dcbae83312658431
arifrumel400 <arif.rumel400@gmail.com>
2021-03-25 00:49:46 +0600

    delete an order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -105,3 +105,17 @@ async function updateStock(id, quantity){
 
     await product.save({validateBeforeSave: false})
 } 
+//Delete order => /admin/order/:id
+exports.deleteOrder = catchAsyncErrors (async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if(!order){
+        return next(new ErrorHandler('No Order found with that id', 404))
+    }
+
+    await order.remove()
+
+    res.status(200).json({
+        success: true
+    })
+}) 
