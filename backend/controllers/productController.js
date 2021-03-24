c563334da64dec4272589394bf71b8fadd4f0b4c
arifrumel400 <arif.rumel400@gmail.com>
2021-03-24 11:01:08 +0600

    category search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,6 +22,7 @@ exports.getProduct = catchAsyncErrors (async (req, res, next) => {
     
     const apiFeatures = new APIFeatures (Product.find(), req.query)
           .search()
+          .filter()   
 
       const products = await apiFeatures.query;
 
