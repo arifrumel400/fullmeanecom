6e5e3fb1e5135c01bb09cebe49417547a8390fd9
arifrumel400 <arif.rumel400@gmail.com>
2021-03-24 17:16:09 +0600

    cookie generated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const sendToken = require('../utils/jwtToken')
 
 //Register a user => /register
 exports.registerUser = catchAsyncErrors (async (req, res, next) =>{
@@ -21,4 +22,38 @@ exports.registerUser = catchAsyncErrors (async (req, res, next) =>{
         success: true,
         token
     })
+}) 
+
+//Login user => /login
+exports.loginUser = catchAsyncErrors (async (req, res, next) => {
+    const {email, password} = req.body
+
+    //Check if email and password is entered by user 
+    if(!email || !password){
+        return next(new ErrorHandler('Please enter email and password!', 404))
+
+    }
+
+    //Find user in database
+    const user = await User.findOne({email}).select('+password')
+
+    if(!user) {
+        return next(new ErrorHandler('Invalied or password', 401)) // 401 = un authenticated user
+    }
+
+    //Check if password matched or not 
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('Invalied or password', 401))
+    }
+
+    //const token = user.getJwtToken()
+
+    //res.status(200).json({
+        //success: true,
+        //token
+    //})
+
+    sendToken(user, 200, res)
 }) 
